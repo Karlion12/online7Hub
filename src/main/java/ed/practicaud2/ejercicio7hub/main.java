609d27383fa542041cda1e88cc75b67165642eea
Karlion <karkon58@hotmail.com>
2022-04-25 20:10:08 +0200

    Añadido el metodo imprimir

--- src/main/java/ed/practicaud2/ejercicio7hub/main.java
+++ src/main/java/ed/practicaud2/ejercicio7hub/main.java
@@ -5,6 +5,10 @@ package ed.practicaud2.ejercicio7hub;
 public class main {
 
     public static void main(String[] args) {
+        imprimir();
+    }
+    
+    public static void imprimir(){
         for (int i = 0; i < 100; i++) {
             if(i%5==0 && i%3==0){
                 System.out.println("FizzBuzz");
