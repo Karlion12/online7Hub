7a8f75565938313574d4b7c1e014d979a1986eb0
Karlion <karkon58@hotmail.com>
2022-04-25 20:07:03 +0200

    Subida

---
+++ src/main/java/ed/practicaud2/ejercicio7hub/main.java
@@ -0,0 +1,21 @@
+
+package ed.practicaud2.ejercicio7hub;
+
+
+public class main {
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 100; i++) {
+            if(i%5==0 && i%3==0){
+                System.out.println("FizzBuzz");
+            }else if(i%3==0) {
+                System.out.println("Fizz");
+            }else if(i%5==0){
+                System.out.println("Buzz");
+            }else{
+                System.out.println(i);
+            }
+        }
+    }
+
+}
